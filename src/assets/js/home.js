9c65aff39a760ba5dc6d7a3477ee11097a1cd3d8
evelyn_zhang <1054982653@qq.com>
2016-12-28 10:18:18 +0800

    form style for iPhone

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -1,10 +1,10 @@
 ;(function(){
 
-    var louis = function(){
+    var controller = function(){
 
     };
     //init
-    louis.prototype.init = function(){
+    controller.prototype.init = function(){
         var self = this;
         //    loading first
         var baseurl = 'dist/images/';
@@ -29,11 +29,12 @@
         //});
 
         //self.welcomePage();
+        self.orderForm();
 
 
     };
     //welcome page
-    louis.prototype.welcomePage = function(){
+    controller.prototype.welcomePage = function(){
         var self = this;
         Common.gotoPin(0);
         var mySwiper = new Swiper ('.swiper-container', {
@@ -49,12 +50,20 @@
 
             // And if we need scrollbar
             //scrollbar: '.swiper-scrollbar',
-        })
+        });
+
+        $('.btn-buy').on('touchstart',function(){
+        //    select product
+        //    product name, product price
+            self.orderForm();
+        });
 
     };
 
+
+
     //update greeting card value
-    louis.prototype.updateFormValue = function(toUser,letterContent,fromUser,isDisabled){
+    controller.prototype.updateFormValue = function(toUser,letterContent,fromUser,isDisabled){
         var self = this;
         var toUserEle = document.getElementById('input-name-1'),
             letterEle = document.getElementById('l-content'),
@@ -74,173 +83,9 @@
         fromUserEle.disabled = isDisabled;
     };
 
-    //write card with words
-    louis.prototype.writeCard = function(){
-        var self = this;
-
+    //fill the order information
+    controller.prototype.orderForm = function(){
         Common.gotoPin(1);
-
-        var toUserEle = $('#input-name-1'),
-            letterEle = $('#l-content'),
-            fromUserEle = $('#input-name-2');
-
-        var btnTextEle = $('.btn-postcard span');
-
-        var i=0;
-        //Form value
-        var initFormVal = [
-            {
-                to:'路易十三的旧友新交',
-                content:'再造100年，\n为你献上跨越世纪的祝福！',
-                from:'路易十三'
-            },
-            {
-                to:'您想发送给谁？',
-                content:'写下您的祝福',
-                from:'请签下您的名字'
-            }
-        ];
-
-        //init form value first
-        self.updateFormValue(initFormVal[i].to,initFormVal[i].content,initFormVal[i].from,true);
-
-        //button
-        var btnText = ['生成我的祝福','提交','分享'];
-        btnTextEle[0].innerText = btnText[i];
-
-        var firstStep = true,
-            secondStep = false,
-            thirdStep = false;
-
-        //test
-        //var firstStep = false,
-        //    secondStep = false,
-        //    thirdStep = true;
-
-        //clean touser value
-        var isCleanFirst = true;
-        toUserEle.on('focus',function(){
-            if(!isCleanFirst) return;
-            if(secondStep){
-                toUserEle.val('');
-                isCleanFirst = false;
-            }
-        });
-
-        //clean letterEle value
-        var isCleanSecond = true;
-        letterEle.on('focus',function(){
-            if(!isCleanSecond) return;
-            if(isCleanSecond){
-                letterEle.val('');
-                isCleanSecond = false;
-            }
-        });
-
-        //clean fromuser value
-        var isCleanThird = true;
-        fromUserEle.on('focus',function(){
-            if(!isCleanThird) return;
-            if(secondStep){
-                fromUserEle.val('');
-                isCleanThird = false;
-            }
-        });
-
-
-        letterEle.on('keydown',function(e){
-            var curVal = letterEle.val();
-            if(e.keyCode==13 || e.which==13){
-                var a = curVal.split('\n');
-                if(a.length>2){
-                    return false;
-                }
-            }
-        });
-
-        //send product and words to backend
-        $('.btn-postcard').on('touchstart',function(){
-            if(firstStep){
-                _hmt.push(['_trackEvent', 'buttons', 'click', '生成我的祝福']);
-                firstStep = false;
-                i=1;
-                self.updateFormValue(initFormVal[i].to,initFormVal[i].content,initFormVal[i].from,false);
-                btnTextEle[0].innerText = btnText[i];
-                secondStep = true;
-                return;
-            };
-
-            var toUserVal = toUserEle.val();
-            var letterEleVal = letterEle.val();
-            var fromuserVal = fromUserEle.val();
-
-
-            if(secondStep){
-                _hmt.push(['_trackEvent', 'buttons', 'click', '提交']);
-            //    validation first,then submit api
-                if(isCleanSecond || isCleanSecond || isCleanSecond){
-                    Common.alertBox.add('好友的名字、祝福、落款缺一不可，请您补充完整');
-                    return;
-                };
-
-                if(toUserVal && letterEleVal && fromuserVal){
-                    //fromname  toname message
-                    Api.saveCard({
-                        message:letterEleVal,
-                        toname:toUserVal,
-                        fromname:fromuserVal
-
-                    },function(data){
-                        if(data.status==1){
-                            secondStep = false;
-                            thirdStep = true;
-
-                            //disable the form
-                            self.updateFormValue(toUserVal,letterEleVal,fromuserVal,false);
-                            btnTextEle[0].innerText = btnText[2];
-
-                            //    start to activate share
-                            var cardId = data.msg;
-                            weixinshare({
-                                title1: '再造100年，为你献上跨世纪的祝福！',
-                                title2: '再造100年，为你献上跨世纪的祝福！',
-                                des: '点亮光感之轮',
-                                link: window.location.origin+'/seasonalgreeting/card.html?cardid='+cardId,
-                                img: 'http://louisxiii-cognac.samesamechina.com/seasonalgreeting/dist/images/share.jpg'
-                            },function(){
-
-                            });
-
-                        }
-
-                        if(data.status !==1){
-                            Common.alertBox.add(data.msg);
-                        }
-
-                    });
-                }else{
-                    Common.alertBox.add('好友的名字、祝福、落款缺一不可，请您补充完整');
-                }
-
-
-
-                return;
-
-            };
-
-            if(thirdStep){
-                _hmt.push(['_trackEvent', 'buttons', 'click', '分享']);
-            //    show share tips
-               $('#popup-share').addClass('show');
-
-
-            }
-
-
-        });
-
-
-
     };
 
 
@@ -248,8 +93,8 @@
     //dom ready
     $(document).ready(function(){
 
-        var seasonGreeting = new louis();
-        seasonGreeting.init();
+        var valentino = new controller();
+        valentino.init();
 
 
     });
